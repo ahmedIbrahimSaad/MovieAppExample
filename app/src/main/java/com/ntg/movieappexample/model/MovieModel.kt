db672870f47f2963acf07cc91bb48582d327ca3d
Ahmed Ibrahim Saad Ibrahim <asibrahim@ntgclarity.com>
2019-12-20 21:53:31 +0200

    add moviedetailsfragment

---
+++ app/src/main/java/com/ntg/movieappexample/model/MovieModel.kt
@@ -0,0 +1,7 @@
+package com.ntg.movieappexample.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MovieModel (val name:String):Parcelable
--- app/src/main/java/com/ntg/movieappexample/presentation/MainActivity.kt
+++ app/src/main/java/com/ntg/movieappexample/presentation/MainActivity.kt
@@ -3,11 +3,20 @@ package com.ntg.movieappexample.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ntg.movieappexample.R
+import com.ntg.movieappexample.model.MovieModel
+import com.ntg.movieappexample.presentation.details.MovieDetailsFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun drawDetailsFragment( model: MovieModel){
+        val fragment= MovieDetailsFragment()
+        val bundle=Bundle()
+        bundle.putParcelable("movia",model)
+        fragment.setArguments(bundle)
+        supportFragmentManager.beginTransaction().replace(R.id.frame,fragment,"movie details").commit()
     }
 }
